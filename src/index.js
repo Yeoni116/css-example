e61d4fa6e6974b9e9185a9a1f2a6c6fc10971c20
Yeoni116 <158430363+Yeoni116@users.noreply.github.com>
2024-02-01 15:32:00 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppClass from './AppClass';
 import App from './button/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
